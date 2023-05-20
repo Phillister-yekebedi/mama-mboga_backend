b220e79a93294772eeb6f34e3dcf378af5b9cc4c
orishaba-mercy <orishabamercybanga@gmail.com>
2023-05-20 21:02:39 +0300

    added return changes

--- products.py
+++ products.py
@@ -6,16 +6,29 @@ class Cart:
             if product_name in self.inventory[category]:
                 self.inventory[category][product_name]["price"] = price
                 self.inventory[category][product_name]["quantity"] += quantity
+                return f"{quantity} {product_name} added to {category} category"
             else:
                 self.inventory[category][product_name] = {"price": price, "quantity": quantity}
+                return f"{product_name} added to {category} category"
         else:
             self.inventory[category] = {product_name: {"price": price, "quantity": quantity}}
+            return f"{category} category created with {product_name} product"
     def remove_product(self, category, product_name):
         if category in self.inventory and product_name in self.inventory[category]:
             del self.inventory[category][product_name]
+            return f"{product_name} removed from {category} category"
+        elif category not in self.inventory:
+            return f"{category} category does not exist"
+        else:
+            return f"{product_name} does not exist in {category} category"
     def update_product_quantity(self, category, product_name, quantity):
         if category in self.inventory and product_name in self.inventory[category]:
             self.inventory[category][product_name]["quantity"]= quantity
+            return f"{product_name} quantity updated to {quantity} in {category} category"
+        elif category not in self.inventory:
+            return f"{category} category does not exist"
+        else:
+            return f"{product_name} does not exist in {category} category"
 cart1 = Cart()
 cart2 = Cart()
 cart1.add_product("fruits", "mangoes", 3, 40)
@@ -25,3 +38,7 @@ cart2.remove_product("vegetables", "Kales")
 cart2.update_product_quantity("vegitables", "sukuma", 4)
 print(cart1.inventory)
 print(cart2.inventory)
+
+
+
+
